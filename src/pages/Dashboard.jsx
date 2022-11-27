d38fd19b3225e4de61cf4e74719812a5485b87e4
rudalson <rudalson@gmail.com>
2022-11-27 13:40:41 +0900

    ✨ added: 2 chart in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import '../styles/dashboard.css';
 import SingleCard from '../components/reusable/SingleCard';
+import MileChart from '../charts/MileChart';
+import CarStatsChart from '../charts/CarStatsChart';
 
 const carObj = {
   title: 'Total Cars',
@@ -36,6 +38,18 @@ const Dashboard = () => {
           <SingleCard item={tripObj} />
           <SingleCard item={clientObj} />
           <SingleCard item={distanceObj} />
+        </div>
+
+        <div className='statics'>
+          <div className='stats'>
+            <h3 className='stats__title'>Miles Statistics</h3>
+            <MileChart />
+          </div>
+
+          <div className='stats'>
+            <h3 className='stats__title'>Car Statistics</h3>
+            <CarStatsChart />
+          </div>
         </div>
       </div>
     </div>
