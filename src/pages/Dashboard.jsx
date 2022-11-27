57a6d2e4f64664b520548356d5985c802f3128d9
rudalson <rudalson@gmail.com>
2022-11-27 13:29:47 +0900

    ✨ added: single card component in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
+import '../styles/dashboard.css';
+import SingleCard from '../components/reusable/SingleCard';
+
+const carObj = {
+  title: 'Total Cars',
+  totalNumber: 750,
+  icon: 'ri-police-car-line',
+};
+
+const tripObj = {
+  title: 'Daily Trips',
+  totalNumber: 1697,
+  icon: 'ri-steering-2-line',
+};
+
+const clientObj = {
+  title: 'Clients Annually',
+  totalNumber: '85k',
+  icon: 'ri-user-line',
+};
+
+const distanceObj = {
+  title: 'Kilometers Daily',
+  totalNumber: 2167,
+  icon: 'ri-timer-flash-line',
+};
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className='dashboard'>
+      <div className='dashboard__wrapper'>
+        <div className='dashboard__cards'>
+          <SingleCard item={carObj} />
+          <SingleCard item={tripObj} />
+          <SingleCard item={clientObj} />
+          <SingleCard item={distanceObj} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
